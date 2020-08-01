253b41072e629dc53e50b95077ab553b83a90cce
zach-he <971499086@qq.com>
2020-08-01 18:15:22 +0800

    [zach]feature:add should_return_illegal_operation_exception_when_update_given_id_not_equals_updated_employee_id

--- src/main/java/com/thoughtworks/springbootemployee/config/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/springbootemployee/config/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.config;
 
+import com.thoughtworks.springbootemployee.exception.IllegalOperationException;
 import com.thoughtworks.springbootemployee.exception.NotSuchDataException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -8,11 +9,19 @@ import org.springframework.web.bind.annotation.*;
 public class GlobalExceptionHandler {
 
     public static final String NOT_SUCH_DATA = "not such data";
+    public static final String ILLEGAL_OPERATION_EXCEPTION = "IllegalOperationException";
 
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotSuchDataException.class)
     String handleNotSuchDataException() {
         return NOT_SUCH_DATA;
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(IllegalOperationException.class)
+    String handleIllegalOperationException(){
+        return ILLEGAL_OPERATION_EXCEPTION;
     }
 }
--- src/test/java/com/thoughtworks/springbootemployee/exception/NotSuchDataExceptionTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/exception/NotSuchDataExceptionTest.java
@@ -36,4 +36,20 @@ public class NotSuchDataExceptionTest {
         //then
         assertEquals(NotSuchDataException.class, exception.getClass());
     }
+
+    @Test
+    void should_return_illegal_operation_exception_when_update_given_id_not_equals_updated_employee_id() {
+        //given
+        EmployeeRepository mockedEmployeeRepository = mock(EmployeeRepository.class);
+        EmployeeService employeeService = new EmployeeService(mockedEmployeeRepository);
+        int id = 1;
+        Employee employee = new Employee(2, "zach", 18, "male", 1000);
+
+        //when
+        Throwable exception = assertThrows(IllegalOperationException.class,
+                () -> employeeService.updateEmployee(1, employee));
+
+        //then
+        assertEquals(IllegalOperationException.class,exception);
+    }
 }
